330ea87a3e824cf7a8f304cf4941482560f92a97
Jason R. Coombs <jaraco@jaraco.com>
2018-05-17 15:03:38 -0400

    Add tests

---
+++ importlib_metadata/tests/test_main.py
@@ -0,0 +1,23 @@
+import unittest
+import re
+import importlib
+
+import importlib_metadata
+
+
+class BasicTests(unittest.TestCase):
+    version_pattern = r'\d+\.\d+(\.\d)?'
+
+    def test_retrieves_version_of_self(self):
+        dist = importlib_metadata.Distribution.for_module(importlib_metadata)
+        assert isinstance(dist.version, str)
+        assert re.match(self.version_pattern, dist.version)
+
+    def test_retrieves_version_of_pip(self):
+        """
+        Assume pip is installed and retrieve the version of pip.
+        """
+        pip = importlib.import_module('pip')
+        dist = importlib_metadata.Distribution.for_module(pip)
+        assert isinstance(dist.version, str)
+        assert re.match(self.version_pattern, dist.version)
