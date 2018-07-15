6cc702c3e314348af0aeec0be13c97a226f126d3
Barry Warsaw <barry@python.org>
2018-07-15 01:11:47 +0000

    Proposed high level API

--- importlib_metadata/tests/test_main.py
+++ importlib_metadata/tests/test_main.py
@@ -1,5 +1,5 @@
-import unittest
 import re
+import unittest
 import importlib
 
 import importlib_metadata
@@ -14,9 +14,7 @@ class BasicTests(unittest.TestCase):
         assert re.match(self.version_pattern, dist.version)
 
     def test_retrieves_version_of_pip(self):
-        """
-        Assume pip is installed and retrieve the version of pip.
-        """
+        # Assume pip is installed and retrieve the version of pip.
         pip = importlib.import_module('pip')
         dist = importlib_metadata.Distribution.from_module(pip)
         assert isinstance(dist.version, str)
@@ -29,3 +27,27 @@ class BasicTests(unittest.TestCase):
     def test_for_module_by_name(self):
         name = 'importlib_metadata'
         importlib_metadata.Distribution.from_named_module(name)
+
+
+class APITests(unittest.TestCase):
+    version_pattern = r'\d+\.\d+(\.\d)?'
+
+    def test_retrieves_version_of_self(self):
+        version = importlib_metadata.version(importlib_metadata)
+        assert isinstance(version, str)
+        assert re.match(self.version_pattern, version)
+
+    def test_retrieves_version_of_pip(self):
+        # Assume pip is installed and retrieve the version of pip.
+        pip = importlib.import_module('pip')
+        version = importlib_metadata.version(pip)
+        assert isinstance(version, str)
+        assert re.match(self.version_pattern, version)
+
+    def test_for_name_does_not_exist(self):
+        with self.assertRaises(importlib_metadata.PackageNotFound):
+            importlib_metadata.distribution('does-not-exist')
+
+    def test_for_module_by_name(self):
+        name = 'importlib_metadata'
+        importlib_metadata.distribution(name)
