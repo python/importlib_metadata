aab98c58f8f14626afc47ef31f21dba017fdc99c
Barry Warsaw <barry@python.org>
2018-09-10 11:45:26 -0700

    Add importlib_metadata.entry_points()
    * Move API tests to their own module.
    * Add changelog entries.
    * Bump version.
    * Update tox for Python 3.8

--- importlib_metadata/tests/test_main.py
+++ importlib_metadata/tests/test_main.py
@@ -29,30 +29,6 @@ class BasicTests(unittest.TestCase):
         importlib_metadata.Distribution.from_named_module(name)
 
 
-class APITests(unittest.TestCase):
-    version_pattern = r'\d+\.\d+(\.\d)?'
-
-    def test_retrieves_version_of_self(self):
-        version = importlib_metadata.version(importlib_metadata)
-        assert isinstance(version, str)
-        assert re.match(self.version_pattern, version)
-
-    def test_retrieves_version_of_pip(self):
-        # Assume pip is installed and retrieve the version of pip.
-        pip = importlib.import_module('pip')
-        version = importlib_metadata.version(pip)
-        assert isinstance(version, str)
-        assert re.match(self.version_pattern, version)
-
-    def test_for_name_does_not_exist(self):
-        with self.assertRaises(importlib_metadata.PackageNotFound):
-            importlib_metadata.distribution('does-not-exist')
-
-    def test_for_module_by_name(self):
-        name = 'importlib_metadata'
-        importlib_metadata.distribution(name)
-
-
 class ImportTests(unittest.TestCase):
     def test_import_nonexistent_module(self):
         """
