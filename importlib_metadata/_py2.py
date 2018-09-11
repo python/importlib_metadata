436cc2c86e4bbd78a4e06fe35205b5c7ac932ebd
Barry Warsaw <barry@python.org>
2018-09-11 14:17:04 +0000

    Support Python 2

--- importlib_metadata/_py2.py
+++ importlib_metadata/_py2.py
@@ -1,12 +1,14 @@
 import os
 import re
 import sys
-import pathlib
+import errno
+import codecs
 import itertools
-import contextlib
 
 from .api import Distribution
-from configparser import ConfigParser
+from ConfigParser import SafeConfigParser
+from cStringIO import StringIO
+from pathlib2 import Path
 
 
 class MetadataPathFinder:
@@ -16,14 +18,16 @@ class MetadataPathFinder:
     PathFinder find_distribution.
     """
     @staticmethod
-    def find_spec(*args, **kwargs):
+    def find_module(*args, **kwargs):
         return None
 
     @classmethod
     def find_distribution(cls, name):
         paths = cls._search_paths(name)
         dists = map(PathDistribution, paths)
-        return next(dists, None)
+        if len(dists) > 0:
+            return dists[0]
+        return None
 
     @classmethod
     def _search_paths(cls, name):
@@ -32,20 +36,19 @@ class MetadataPathFinder:
         """
         return itertools.chain.from_iterable(
             cls._search_path(path, name)
-            for path in map(pathlib.Path, sys.path)
+            for path in map(Path, sys.path)
             )
 
     @classmethod
     def _search_path(cls, root, name):
-        if not root.is_dir():
-            return ()
-        return (
-            item
-            for item in root.iterdir()
-            if item.is_dir()
-            and str(item.name).startswith(name)
-            and re.match(rf'{name}(-.*)?\.(dist|egg)-info', str(item.name))
-            )
+        if root.is_dir():
+            for item in root.iterdir():
+                item_name = str(item.name)
+                if (item.is_dir()
+                    and item_name.startswith(name)
+                    and re.match(r'{}(-.*)?\.(dist|egg)-info'.format(name),
+                                 item_name)):
+                    yield item
 
 
 class PathDistribution(Distribution):
@@ -59,10 +62,14 @@ class PathDistribution(Distribution):
         """
         Attempt to load metadata given by the name. Return None if not found.
         """
-        filename = os.path.join(self.path, name)
-        with contextlib.suppress(FileNotFoundError):
-            with open(filename, encoding='utf-8') as fp:
+        filename = os.path.join(str(self.path), name)
+        try:
+            with codecs.open(filename, encoding='utf-8') as fp:
                 return fp.read()
+        except IOError as error:
+            if error.errno != errno.ENOENT:
+                raise                               # pragma: nocover
+            return None
 
 
 def entry_points(name):
@@ -71,6 +78,6 @@ def entry_points(name):
     as_string = distribution(name).load_metadata('entry_points.txt')
     # 2018-09-10(barry): Should we provide any options here, or let the caller
     # send options to the underlying ConfigParser?   For now, YAGNI.
-    config = ConfigParser()
-    config.read_string(as_string)
+    config = SafeConfigParser()
+    config.readfp(StringIO(as_string))
     return config
