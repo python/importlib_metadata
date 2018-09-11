88ff6e837a7eef1b6f651a25c23a43db01496dff
Jason R. Coombs <jaraco@jaraco.com>
2018-09-11 19:34:43 -0400

    Configure project to set the version.txt based on scm metadata (tags, commits, dirty).

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
+from __future__ import print_function
+
 from setuptools import setup
+import setuptools_scm
+
+with open('importlib_metadata/version.txt', 'w') as f:
+    print(setuptools_scm.get_version(), file=f)
+
 setup()
