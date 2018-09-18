0ed71e1d13a06ffba650ec63861ac6078f0fd5db
Jason R. Coombs <jaraco@jaraco.com>
2018-09-18 01:23:38 +0000

    Sync _py2 and _py3

--- importlib_metadata/_common.py
+++ importlib_metadata/_common.py
@@ -1,38 +1,59 @@
-import os
+from __future__ import unicode_literals, absolute_import
+
+import io
 import re
 import sys
-import errno
-import codecs
 import itertools
 
 from .api import Distribution
-from ConfigParser import SafeConfigParser
-from cStringIO import StringIO
 from importlib import import_module
-from pathlib2 import Path
 from zipfile import ZipFile
 
+if sys.version_info >= (3,):  # pragma: nocover
+    from contextlib import suppress
+    from configparser import ConfigParser
+    from pathlib import Path
+else:  # pragma: nocover
+    from ConfigParser import SafeConfigParser as ConfigParser
+    from contextlib2 import suppress  # noqa
+    from itertools import imap as map  # type: ignore
+    from pathlib2 import Path
 
-class MetadataPathFinder:
-    """A degenerate finder for distribution packages.
+    FileNotFoundError = IOError, OSError
+
+
+class NullFinder:
+    @staticmethod
+    def find_spec(*args, **kwargs):
+        return None
+
+    # In Python 2, the import system requires finders
+    # to have a find_module() method, but this usage
+    # is deprecated in Python 3 in favor of find_spec().
+    # For the purposes of this finder (i.e. being present
+    # on sys.meta_path but having no other import
+    # system functionality), the two methods are identical.
+    find_module = find_spec
+
+
+class MetadataPathFinder(NullFinder):
+    """A degenerate finder for distribution packages on the file system.
 
     This finder supplies only a find_distribution() method for versions
     of Python that do not have a PathFinder find_distribution().
     """
-    @staticmethod
-    def find_module(*args, **kwargs):
-        return None
 
     @classmethod
     def find_distribution(cls, name):
         paths = cls._search_paths(name)
         dists = map(PathDistribution, paths)
-        if len(dists) > 0:
-            return dists[0]
-        return None
+        return next(dists, None)
 
     @classmethod
     def _search_paths(cls, name):
+        """
+        Find metadata directories in sys.path heuristically.
+        """
         return itertools.chain.from_iterable(
             cls._search_path(path, name)
             for path in map(Path, sys.path)
@@ -40,36 +61,57 @@ class MetadataPathFinder:
 
     @classmethod
     def _search_path(cls, root, name):
-        if root.is_dir():
-            for item in root.iterdir():
-                item_name = str(item.name)
-                if (item.is_dir()
-                    and item_name.startswith(name)
-                    and re.match(r'{}(-.*)?\.(dist|egg)-info'.format(name),
-                                 item_name)):
-                    yield item
+        if not root.is_dir():
+            return ()
+        return (
+            item
+            for item in root.iterdir()
+            if item.is_dir()
+            and str(item.name).startswith(name)
+            and re.match(
+                r'{name}(-.*)?\.(dist|egg)-info'.format(name=name),
+                str(item.name),
+                )
+            )
 
 
-class WheelMetadataFinder:
+class PathDistribution(Distribution):
+    def __init__(self, path):
+        """Construct a distribution from a path to the metadata directory."""
+        self._path = path
+
+    def load_metadata(self, name):
+        """Attempt to load metadata given by the name.
+
+        :param name: The name of the distribution package.
+        :return: The metadata string if found, otherwise None.
+        """
+        with suppress(FileNotFoundError):
+            with self._path.joinpath(name).open(encoding='utf-8') as fp:
+                return fp.read()
+        return None
+
+
+class WheelMetadataFinder(NullFinder):
     """A degenerate finder for distribution packages in wheels.
 
     This finder supplies only a find_distribution() method for versions
     of Python that do not have a PathFinder find_distribution().
     """
-    @staticmethod
-    def find_module(*args, **kwargs):
-        return None
-
     @classmethod
     def find_distribution(cls, name):
         try:
             module = import_module(name)
         except ImportError:
             return None
+
+        # Python 2: allow modules without a loader.
+        # Only modules with a __loader__.archive are relevant.
         loader = getattr(module, '__loader__', None)
-        if loader is None:
-            return
-        archive = loader.archive
+        archive = getattr(loader, 'archive', None)
+        if archive is None:
+            return None
+
         try:
             name, version = Path(archive).name.split('-')[0:2]
         except ValueError:
@@ -82,27 +124,6 @@ class WheelMetadataFinder:
             zi = zf.getinfo('{}/{}'.format(dist_info, 'METADATA'))
             metadata = zf.read(zi).decode('utf-8')
         return WheelDistribution(archive, dist_info, metadata)
-
-
-class PathDistribution(Distribution):
-    def __init__(self, path):
-        """Construct a distribution from a path to the metadata directory."""
-        self.path = path
-
-    def load_metadata(self, name):
-        """Attempt to load metadata given by the name.
-
-        :param name: The name of the distribution package.
-        :return: The metadata string if found, otherwise None.
-        """
-        filename = os.path.join(str(self.path), name)
-        try:
-            with codecs.open(filename, encoding='utf-8') as fp:
-                return fp.read()
-        except IOError as error:
-            if error.errno != errno.ENOENT:
-                raise                               # pragma: nocover
-            return None
 
 
 class WheelDistribution(Distribution):
@@ -120,11 +141,10 @@ class WheelDistribution(Distribution):
         if name == 'METADATA':
             return self._metadata
         with ZipFile(self._archive) as zf:
-            try:
+            with suppress(KeyError):
                 as_bytes = zf.read('{}/{}'.format(self._dist_info, name))
-            except KeyError:
-                return None
-            return as_bytes.decode('utf-8')
+                return as_bytes.decode('utf-8')
+        return None
 
 
 def entry_points(name):
@@ -139,6 +159,10 @@ def entry_points(name):
     as_string = distribution(name).load_metadata('entry_points.txt')
     # 2018-09-10(barry): Should we provide any options here, or let the caller
     # send options to the underlying ConfigParser?   For now, YAGNI.
-    config = SafeConfigParser()
-    config.readfp(StringIO(as_string))
+    config = ConfigParser()
+    try:
+        config.read_string(as_string)
+    except AttributeError:  # pragma: nocover
+        # Python 2 has no read_string
+        config.readfp(io.StringIO(as_string))
     return config
