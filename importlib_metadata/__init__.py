cc306e1c3786c03b3c8d89ce74ebb73c4904c977
Jason R. Coombs <jaraco@jaraco.com>
2018-09-19 08:24:18 -0400

    Rename _common to _hooks

--- importlib_metadata/__init__.py
+++ importlib_metadata/__init__.py
@@ -1,6 +1,6 @@
 from .api import Distribution, PackageNotFoundError              # noqa: F401
 from .api import distribution, entry_points, resolve, version
-from ._common import _install
+from ._hooks import _install
 
 __all__ = [
     'distribution',
