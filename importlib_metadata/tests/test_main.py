0e945cf6cc45c899329d692571963eed6a76de5f
Jason R. Coombs <jaraco@jaraco.com>
2018-11-10 10:06:12 -0500

    Add test capturing failed expectation. Ref #20.

--- importlib_metadata/tests/test_main.py
+++ importlib_metadata/tests/test_main.py
@@ -1,7 +1,23 @@
+from __future__ import unicode_literals
+
 import re
+import sys
+import shutil
+import tempfile
 import unittest
 import importlib
+import contextlib
 import importlib_metadata
+
+try:
+    from contextlib import ExitStack
+except ImportError:
+    from contextlib2 import ExitStack
+
+try:
+    import pathlib
+except ImportError:
+    import pathlib2 as pathlib
 
 from importlib_metadata import _hooks
 
@@ -42,3 +58,43 @@ class ImportTests(unittest.TestCase):
 
     def test_resolve_invalid(self):
         self.assertRaises(ValueError, importlib_metadata.resolve, 'bogus.ep')
+
+
+class NameNormalizationTests(unittest.TestCase):
+    @staticmethod
+    @contextlib.contextmanager
+    def site_dir():
+        tmpdir = tempfile.mkdtemp()
+        sys.path[:0] = [tmpdir]
+        try:
+            yield pathlib.Path(tmpdir)
+        finally:
+            sys.path.remove(tmpdir)
+            shutil.rmtree(tmpdir)
+
+    def setUp(self):
+        self.fixtures = ExitStack()
+        self.addCleanup(self.fixtures.close)
+        self.site_dir = self.fixtures.enter_context(self.site_dir())
+
+    @staticmethod
+    def pkg_with_mixed_case(site_dir):
+        """
+        Create minimal metadata for a package with mixed case
+        in the name.
+        """
+        metadata_dir = site_dir / 'CherryPy.dist-info'
+        metadata_dir.mkdir()
+        metadata = metadata_dir / 'METADATA'
+        with metadata.open('w') as strm:
+            strm.write('Version: 1.0\n')
+        return 'CherryPy'
+
+    def test_dist_name_found_as_any_case(self):
+        """
+        Ensure the metadata loads when queried with any case.
+        """
+        pkg_name = self.pkg_with_mixed_case(self.site_dir)
+        assert importlib_metadata.version(pkg_name) == '1.0'
+        assert importlib_metadata.version(pkg_name.lower()) == '1.0'
+        assert importlib_metadata.version(pkg_name.upper()) == '1.0'
