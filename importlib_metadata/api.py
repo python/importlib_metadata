bb5f53a634ee7fdbe8446de19ba757fbf056a96f
Jason R. Coombs <jaraco@jaraco.com>
2018-12-05 21:51:22 -0500

    Convert staticmethod to global function

--- importlib_metadata/api.py
+++ importlib_metadata/api.py
@@ -1,5 +1,5 @@
-import io
 import abc
+import io
 import sys
 import email
 
@@ -68,22 +68,22 @@ class Distribution:
         metadata.  See PEP 566 for details.
         """
         text = self.read_text('METADATA') or self.read_text('PKG-INFO')
-        return self.__email_message_from_string(text)
-
-    @staticmethod
-    def __email_message_from_string(text):  # pragma: nocover
-        """Work around bug that email.message_from_string cannot
-        handling Unicode on Python 2.
-        """
-        if sys.version_info < (3,):
-            buffer = io.StringIO(text)
-            return email.message_from_file(buffer)
-        return email.message_from_string(text)
+        return _email_message_from_string(text)
 
     @property
     def version(self):
         """Return the 'Version' metadata for the distribution package."""
         return self.metadata['Version']
+
+
+def _email_message_from_string(text):  # pragma: nocover
+    """Work around bug that email.message_from_string cannot
+    handle Unicode on Python 2.
+    """
+    if sys.version_info < (3,):
+        buffer = io.StringIO(text)
+        return email.message_from_file(buffer)
+    return email.message_from_string(text)
 
 
 def distribution(package):
