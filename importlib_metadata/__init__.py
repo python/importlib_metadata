fa54a01a282da90c8b7445f84af648bbcf124e14
Jason R. Coombs <jaraco@jaraco.com>
2018-12-22 19:15:17 +0000

    Revamp entrypoints

--- importlib_metadata/__init__.py
+++ importlib_metadata/__init__.py
@@ -1,5 +1,5 @@
 from .api import distribution, Distribution, PackageNotFoundError  # noqa: F401
-from .api import metadata, entry_points, resolve, version, read_text
+from .api import metadata, entry_points, version, read_text
 
 # Import for installation side-effects.
 from . import _hooks  # noqa: F401
@@ -8,7 +8,6 @@ from . import _hooks  # noqa: F401
 __all__ = [
     'metadata',
     'entry_points',
-    'resolve',
     'version',
     'read_text',
     ]
