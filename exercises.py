f0d021ab817bbca135924a58d756ffa73a58b48f
Jason R. Coombs <jaraco@jaraco.com>
2021-06-26 19:08:50 -0400

    ⚫ Fade to black.

--- exercises.py
+++ exercises.py
@@ -5,12 +5,14 @@ from pytest_perf.deco import extras
 def discovery_perf():
     "discovery"
     import importlib_metadata  # end warmup
+
     importlib_metadata.distribution('ipython')
 
 
 def entry_points_perf():
     "entry_points()"
     import importlib_metadata  # end warmup
+
     importlib_metadata.entry_points()
 
 
@@ -18,6 +20,7 @@ def entry_points_perf():
 def cached_distribution_perf():
     "cached distribution"
     import importlib_metadata
+
     importlib_metadata.distribution('ipython')  # end warmup
     importlib_metadata.distribution('ipython')
 
@@ -27,6 +30,7 @@ def uncached_distribution_perf():
     "uncached distribution"
     import importlib
     import importlib_metadata
+
     # end warmup
     importlib.invalidate_caches()
     importlib_metadata.distribution('ipython')
