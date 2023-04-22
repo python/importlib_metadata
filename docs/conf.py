a4c314793e0ab96cab1b711b4c4150327642356a
Jason R. Coombs <jaraco@jaraco.com>
2023-04-22 08:31:42 -0400

    Remove nitpick_ignore no longer needed.

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,6 @@ nitpick_ignore = [
     # Workaround for #316
     ('py:class', 'importlib_metadata.EntryPoints'),
     ('py:class', 'importlib_metadata.PackagePath'),
-    ('py:class', 'importlib_metadata.PathDistribution'),
     ('py:class', 'importlib_metadata.SelectableGroups'),
     ('py:class', 'importlib_metadata._meta._T'),
     # Workaround for #435
