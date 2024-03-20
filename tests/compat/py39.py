1711b2c1984b2f871fe12c9c14fd7a0b42d32987
Jason R. Coombs <jaraco@jaraco.com>
2024-03-20 15:26:59 -0400

    Need to include names from test.support for py312 compat.

--- tests/compat/py39.py
+++ tests/compat/py39.py
@@ -4,4 +4,6 @@ from jaraco.test.cpython import from_test_support, try_import
 os_helper = try_import('os_helper') or from_test_support(
     'FS_NONASCII', 'skip_unless_symlink'
 )
-import_helper = try_import('import_helper') or from_test_support()
+import_helper = try_import('import_helper') or from_test_support(
+    'modules_setup', 'modules_cleanup'
+)
