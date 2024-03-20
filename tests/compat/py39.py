e30a16d471f62555db5605d5652bede9a1234b1a
Jason R. Coombs <jaraco@jaraco.com>
2024-03-20 06:20:46 -0400

    Consolidated test support logic in jaraco.test.cpython.

--- tests/compat/py39.py
+++ tests/compat/py39.py
@@ -1,18 +1,6 @@
-import types
-
-from jaraco.collections import Projection
+from jaraco.test.cpython import from_test_support, try_import
 
 
-def from_test_support(*names):
-    """
-    Return a SimpleNamespace of names from test.support.
-    """
-    import test.support
-
-    return types.SimpleNamespace(**Projection(names, vars(test.support)))
-
-
-try:
-    from test.support import os_helper  # type: ignore
-except ImportError:
-    os_helper = from_test_support('FS_NONASCII', 'skip_unless_symlink')
+os_helper = try_import('os_helper') or from_test_support(
+    'FS_NONASCII', 'skip_unless_symlink'
+)
