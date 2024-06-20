963f643b1f05effc980bb6df0196c64f6576dfc6
Jason R. Coombs <jaraco@jaraco.com>
2024-06-20 12:39:10 -0400

    gh-120801: Update fixtures.
    Removed unused fixtures relating to cwd. Rely on os_helper.temp_dir where relevant. Renamed to tmp_path to reflect pathlib behavior.

--- tests/compat/py39.py
+++ tests/compat/py39.py
@@ -2,7 +2,7 @@ from jaraco.test.cpython import from_test_support, try_import
 
 
 os_helper = try_import('os_helper') or from_test_support(
-    'FS_NONASCII', 'skip_unless_symlink'
+    'FS_NONASCII', 'skip_unless_symlink', 'temp_dir'
 )
 import_helper = try_import('import_helper') or from_test_support(
     'modules_setup', 'modules_cleanup'
