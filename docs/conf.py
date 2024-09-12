90073b1aa7a49cc5fdbdc0e6e871f39e461b9422
Jason R. Coombs <jaraco@jaraco.com>
2024-09-12 05:01:16 -0400

    Separate bpo from Python issue numbers.

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,12 @@ link_files = {
                 url='https://peps.python.org/pep-{pep_number:0>4}/',
             ),
             dict(
-                pattern=r'(python/cpython#|Python #|py-)(?P<python>\d+)',
+                pattern=r'(python/cpython#|Python #)(?P<python>\d+)',
                 url='https://github.com/python/cpython/issues/{python}',
+            ),
+            dict(
+                pattern=r'bpo-(?P<bpo>\d+)',
+                url='http://bugs.python.org/issue{bpo}',
             ),
         ],
     )
